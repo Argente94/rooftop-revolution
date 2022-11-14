9a00aed7be316f90283e081ece1e57b534234d45
Argente94 <alex.argente94@gmail.com>
2022-11-14 15:30:18 +0100

    Layout + Landing page

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="layout">
+    <Nuxt />
+  </div>
+</template>
+
+<style lang="scss">
+    .layout {
+        min-height: 100vh;
+        overflow: hidden;
+        background-color: $background-color;
+        padding: 30px;
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,30 @@
 <template>
-  <div>
-      <h2>Introduce tu CUPS para descubrir si cumples con los requisitos para sumarte a la revolución de los tejados</h2>
-      <input type="text" placeholder="Introduce el CUPS" @input="onChangeInput">
-      <button :disabled="isDisabled()">Search</button>
+  <div class="container">
+    <div class="stack">
+      <h2 class="heading center">
+        Súmate a la Revolución de los Tejados
+      </h2>
+      <form class="form-card">
+        <div class="stack--small-gap">
+          <p class="text">
+            Introduce tu CUPS y <strong>descubre tu oferta</strong>
+          </p>
+          <div class="columns">
+            <input v-model="input" class="text-field" type="text" placeholder="Introduce el CUPS">
+            <button class="button--primary" :disabled="isButtonDisabled" @click="onClickButton">
+              Busca tu oferta
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+    <div class="container">
+      <div v-show="loading" class="text center">
+        Cargando...
+      </div>
+      <ClientCard v-show="ready" :cups="cups" />
+      <ErrorMessage v-show="error" :msg="errorMessage" />
+    </div>
   </div>
 </template>
 
@@ -11,23 +33,60 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'IndexPage',
-  
-  data() {
+
+  data () {
     return {
-      inputText: '',
+      cups: '',
+      input: '',
+      ready: false,
+      loading: false,
+      error: false,
+      errorMessage: ''
+    }
+  },
+
+  computed: {
+    isButtonDisabled (): boolean {
+      const CUPS_LENGTH = 6
+      return this.input.length !== CUPS_LENGTH
     }
   },
 
   methods: {
-    onChangeInput(event: Event){
-      const target = (event.target as HTMLInputElement);
-      this.inputText = target.value;
-    },
+    async onClickButton (event: Event) {
+      event.preventDefault()
 
-    isDisabled(){
-        const CUPS_LENGTH = 6;
-        return this.inputText.length !== CUPS_LENGTH;
+      this.cups = this.input
+
+      this.ready = false
+      this.error = false
+      this.loading = true
+
+      try {
+        await this.$store.dispatch('fetchClients')
+      } catch (e: any) {
+        this.error = true
+        this.errorMessage = e.message
+      }
+
+      if (!this.error) {
+        try {
+          await this.$store.dispatch('fetchSupplyPoints')
+        } catch (e: any) {
+          this.error = true
+          this.errorMessage = e.message
+        }
+      }
+
+      if (this.$store.getters.getClientByCups(this.cups) === null ||
+      this.$store.getters.getSupplyPointByCups(this.cups) === null) {
+        this.error = true
+        this.errorMessage = 'CUPS not found'
+      }
+
+      this.ready = !this.error
+      this.loading = false
     }
-  },
+  }
 })
 </script>
